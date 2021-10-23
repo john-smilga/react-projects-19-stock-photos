e7b792256645e7f41f94cf8c4f7a8cf5142fde37
john-smilga <johnsmilga@gmail.com>
2021-10-22 19:12:21 -0700

    fixed few bugs

--- src/App.js
+++ src/App.js
@@ -1,68 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import React, { useState, useEffect, useRef } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
-import Photo from './Photo'
-const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import Photo from './Photo';
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(1)
-  const [query, setQuery] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [newImages, setNewImages] = useState(false);
+  const mounted = useRef(false);
+
   const fetchImages = async () => {
-    setLoading(true)
-    let url
-    const urlPage = `&page=${page}`
-    const urlQuery = `&query=${query}`
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
     if (query) {
-      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
     } else {
-      url = `${mainUrl}${clientID}${urlPage}`
+      url = `${mainUrl}${clientID}${urlPage}`;
     }
     try {
-      const response = await fetch(url)
-      const data = await response.json()
+      const response = await fetch(url);
+      const data = await response.json();
       setPhotos((oldPhotos) => {
         if (query && page === 1) {
-          return data.results
+          return data.results;
         } else if (query) {
-          return [...oldPhotos, ...data.results]
+          return [...oldPhotos, ...data.results];
         } else {
-          return [...oldPhotos, ...data]
+          return [...oldPhotos, ...data];
         }
-      })
-      setLoading(false)
+      });
+
+      setNewImages(false);
+      setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      setNewImages(false);
+      setLoading(false);
     }
-  }
+  };
   useEffect(() => {
-    fetchImages()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page])
+    fetchImages();
+  }, [page]);
 
   useEffect(() => {
-    const event = window.addEventListener('scroll', () => {
-      if (
-        (!loading && window.innerHeight + window.scrollY) >=
-        document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => {
-          return oldPage + 1
-        })
-      }
-    })
-    return () => window.removeEventListener('scroll', event)
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => {
+      return oldPage + 1;
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', event);
+    return () => window.removeEventListener('scroll', event);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setPage(() => 1)
-    fetchImages()
-  }
+    e.preventDefault();
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+    }
+    setPage(1);
+  };
   return (
     <main>
       <section className='search'>
@@ -82,13 +99,13 @@ function App() {
       <section className='photos'>
         <div className='photos-center'>
           {photos.map((image, index) => {
-            return <Photo key={index} {...image} />
+            return <Photo key={index} {...image} />;
           })}
         </div>
         {loading && <h2 className='loading'>Loading...</h2>}
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
